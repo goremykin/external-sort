956e712fce0bea36f3fb9dea749c1a4f01c35623
Stepan Goremykin <s.goremykin@proton.me>
2025-01-20 23:30:08 +0100

    Add linux sorter

---
+++ ExternalSort.Sorter/Program.cs
@@ -0,0 +1,25 @@
+﻿using ExternalSort.Sorter;
+
+if (args.Length < 2)
+{
+    Console.WriteLine("Please provide input and output paths");
+    return;
+}
+
+var inputPath = args[0];
+var outputPath = args[1];
+
+if (!File.Exists(inputPath))
+{
+    Console.WriteLine("Invalid input path");
+    return;
+}
+
+var startTimestamp = DateTime.Now;
+var sorter = new LinuxSorter();
+
+await sorter.SortAsync(inputPath, outputPath);
+
+var spent = (DateTime.Now - startTimestamp).TotalMilliseconds;
+
+Console.WriteLine($"Done in {spent} ms");
